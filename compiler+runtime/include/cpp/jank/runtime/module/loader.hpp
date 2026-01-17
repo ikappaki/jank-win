18362d613bae2530d25d8a329cf0f5cff7eb3cb5
ikappaki <ikappaki@users.noreply.github.com>
2026-01-17 12:27:40 +0000

    Add native win32 support for memory mapped files

--- compiler+runtime/include/cpp/jank/runtime/module/loader.hpp
+++ compiler+runtime/include/cpp/jank/runtime/module/loader.hpp
@@ -2,10 +2,22 @@
 
 #include <filesystem>
 
-#include <boost/iostreams/device/mapped_file.hpp>
-
 #include <jank/runtime/object.hpp>
 #include <jtl/result.hpp>
+
+#ifdef _WIN32
+#include <windows.h>
+struct HANDLES
+{
+  HANDLE hFile;
+  HANDLE hMapping;
+  HANDLES() = delete;
+  HANDLES(HANDLE file, HANDLE mapping) : hFile(file), hMapping(mapping) {}
+};
+using file_handle = HANDLES;
+#else
+using file_handle = int;
+#endif
 
 namespace jank::runtime
 {
@@ -70,7 +82,7 @@ namespace jank::runtime::module
     file_view() = default;
     file_view(file_view const &) = delete;
     file_view(file_view &&) noexcept;
-    file_view(boost::iostreams::mapped_file_source mfs, char const * const h, usize const s);
+    file_view(file_handle f, char const * const h, usize const s);
     file_view(jtl::immutable_string const &buff);
     ~file_view();
 
@@ -82,7 +94,7 @@ namespace jank::runtime::module
   private:
     /* In the case where we map a file, we track this information so we can read it and
      * later unmap it. */
-    boost::iostreams::mapped_file_source mfs;
+    std::optional<file_handle> fd{};
     char const *head{};
     usize len{};
 
