ee6e4d4150c08535a3cc0918129684087df54631
ikappaki <ikappaki@users.noreply.github.com>
2026-02-09 07:31:43 +0000

    Remove explicit constructors workaround from prelude.hpp

--- compiler+runtime/include/cpp/jank/prelude.hpp
+++ compiler+runtime/include/cpp/jank/prelude.hpp
@@ -3,14 +3,6 @@
 /* This file is turned into a pre-compiled header which is included at run-time
  * to provide fast access to jank's C++ API. */
 
-#ifdef __MINGW64__
-  // Workaround to make the std::ifstream(const char*) constructor
-  // available to jank
-  #include <fstream>
-std::ifstream __jank_mingw64_ifs("");
-std::ofstream __jank_mingw64_ofs;
-#endif
-
 #include <jank/runtime/convert/builtin.hpp>
 #include <jank/runtime/visit.hpp>
 #include <jank/runtime/context.hpp>
